4c4538080dabd60cd61ae7d63283615deb16c81c
Silverwinterr <Wellmet12>
2021-04-05 15:35:30 +0700

    task 4

--- bulatov_lesson_4/app/src/main/java/com/bulatov/lesson4/ItemDecoration.kt
+++ bulatov_lesson_4/app/src/main/java/com/bulatov/lesson4/ItemDecoration.kt
@@ -7,6 +7,9 @@ import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class ItemDecoration(private val offset : Int) : RecyclerView.ItemDecoration() {
+    companion object {
+        const val LAYOUT_POSITION_LONG = 5
+    }
     override fun getItemOffsets(
         outRect: Rect,
         view: View,
@@ -14,8 +17,22 @@ class ItemDecoration(private val offset : Int) : RecyclerView.ItemDecoration() {
         state: RecyclerView.State
     ) {
        val offsetDp = offset * Resources.getSystem().displayMetrics.density.toInt()
+        val position = parent.getChildAdapterPosition(view)
+        if (position> LAYOUT_POSITION_LONG){
             outRect.top = offsetDp
             outRect.left = offsetDp
-            outRect.right = offsetDp
+            outRect.right = offsetDp }
+        else {
+            if (position % 2 !=0) {
+                outRect.top = offsetDp
+                outRect.left = offsetDp / 2
+                outRect.right = offsetDp
+            }
+            else {
+                outRect.top = offsetDp
+                outRect.left = offsetDp
+                outRect.right = offsetDp / 2
+            }
+        }
     }
 }
